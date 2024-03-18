a1377f704fdc8e98b542fdf0bb9fb5648c6ac2b6
KIEN84 <112917175+Kientv84@users.noreply.github.com>
2024-03-18 22:08:06 +0700

    build header ui log, input, btn search, clear, loading

--- src/componets/Layout/components/Header/index.js
+++ src/componets/Layout/components/Header/index.js
@@ -1,14 +1,28 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
+console.log(images.logo);
 function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        {/* Nội dung */}
-        {/* Nội dung */}
+        <img src={images.logo} alt="Tiktok" />
+        <div className={cx('search')}>
+          <input placeholder="Search accounts and videos" />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-button')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx('action')}></div>
       </div>
     </header>
   );
